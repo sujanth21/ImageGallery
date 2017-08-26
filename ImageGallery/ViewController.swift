fe5c59c5171558cdca71053d00bd38195df3a9c7
Sujanth <sujanth1987@gmail.com>
2017-08-27 00:59:51 +1000

    Back button logic added

--- ImageGallery/ViewController.swift
+++ ImageGallery/ViewController.swift
@@ -10,9 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var backBtn: UIButton!
+    @IBOutlet weak var nextBtn: UIButton!
+    @IBOutlet weak var label: UILabel!
+    
+    var imageArray = ["Image1", "Image2", "Image3", "Image4", "Image5", "Image6"]
+    var currentImage = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        backBtn.isEnabled = false
+        backBtn.alpha = 0.25
+        imageView.image = UIImage(named: "\(imageArray[currentImage])")
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +32,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func backBtnPressed(_ sender: UIButton) {
+        if currentImage > 0 {
+            backBtn.isEnabled = true
+            currentImage -= 1
+            imageView.image = UIImage(named: "\(imageArray[currentImage])")
+        } else {
+            backBtn.isEnabled = false
+            backBtn.alpha = 0.25
+        }
+        
+    }
+    
+    
+    @IBAction func nextBtnPressed(_ sender: UIButton) {
+    }
+    
 }
 
